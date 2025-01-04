4653874f430b98340ce883a0968f6249fbb1cb5c
olgapyatunina2024 <olgapyatunina2024@yandex.by>
2025-01-04 10:23:48 +0500

    Add files via upload

---
+++ test_12_1_1.py
@@ -0,0 +1,23 @@
+import runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        walker = runner.Runner('123')
+        for i in range(10):
+            walker.walk()
+        self.assertEqual(walker.distance, 50)
+
+    def test_run(self):
+        run = runner.Runner('456')
+        for i in range(10):
+            run.run()
+        self.assertEqual(run.distance, 100)
+
+    def test_challenge(self):
+        walker2 = runner.Runner('789')
+        run2 = runner.Runner('000')
+        for i in range(10):
+            walker2.walk()
+            run2.run()
+        self.assertNotEqual(run2.distance, walker2.distance)
